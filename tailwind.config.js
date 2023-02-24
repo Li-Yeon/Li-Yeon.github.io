913def1a0d6ed02efbd73cd0e30cc1cc35b517ec
Li Yeon <nabalu.tech@gmail.com>
2023-02-24 15:48:47 +0800

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Raleway: ['Raleway'],
+        Shantell: ['Shantell Sans'],
+        Quicksand: ['Quicksand']
+
+      }
+    },
+  },
+  plugins: [],
+}
